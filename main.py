82f7c97afc2bfcdbfc66fa4f68e7188d444bdf2f
Joejoe <82959624+joejoe135791@users.noreply.github.com>
2025-01-12 03:07:11 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+import os
+import json
+import sys
+import random
+from tkinter import *
+from tkinter import messagebox 
+from termcolor import cprint
+
+debugKaraokeMode = True
+i = 0
+os.system('color')
+loadedNumberJson = json.load(open("numbers.json"))
+loadedNumberListData = loadedNumberJson["numbers"]
+randomWhileAmount = random.randint(1, 25)
+# Backup old data just in case
+with open("oldNumbers.json", "w") as outfile:
+    json.dump(loadedNumberJson, outfile, indent=4, sort_keys=True)
+
+print("Welcome to the Random Number picker by joejoeVT, designed for karaoke challenges")
+cprint("In case the script ever deletes json data. all data has been backed up to oldNumbers.json. DO NOT RUN THE SCRIPT AGAIN BEFORE VERIFYING THE ORIGINAL JSON FILE!", "light_yellow", attrs=['bold'])
+if debugKaraokeMode == True:
+    cprint("Debugging enabled!")
+    print(f"Amount to randomize: {randomWhileAmount}")
+    print(f"loaded JSON Data\n{loadedNumberListData}")
+
+while (i < randomWhileAmount):
+    i += 1
+    selectedNumber = random.choice(loadedNumberListData)
+    if debugKaraokeMode == True:
+        print(f"While loop has run {i}/{randomWhileAmount} times and has selected {selectedNumber}")
+
+root = Tk()
+root.withdraw()
+root.geometry("300x200")
+  
+messagebox.showinfo(f"Selected Number {selectedNumber}", f"Selected Number: {selectedNumber}") 
+print(f"Selected number {selectedNumber}")
+
+removedNumberList = loadedNumberListData.remove(selectedNumber)
+newJsonData = dict(numbers = removedNumberList)
+with open("numbers.json", "w") as outfile:
+    json.dump(loadedNumberJson, outfile, indent=4, sort_keys=True)
+print(f"{selectedNumber} has been removed from list")
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Random Number Picker
+This is a random number picker I developed specifically to give me a truly random number from a list for my 100 day/100 song karaoke challenge (posting soon). feel free to use and fork however you like
+
+# How to use
+Simply change the data in the '[numbers.json](./numbers.json)' file and add your own numbers
+
+# Future plans
+I don't plan to support this project in the future however I *MAY* add support for something other than numbers
+
+# NO SUPPORT
+This project comes with no support from me as it is a personal project
+
+# License
+This project is licensed under the [GPL-3.0-only](./LICENSE) License. Read LICENSE for more information
