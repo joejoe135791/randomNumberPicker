c16b5585630d3c9e786d935b2da6beb0821e04d9
Joejoe <82959624+joejoe135791@users.noreply.github.com>
2025-01-16 11:46:42 -0800

    hopefully it works

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ debugKaraokeMode = loadedConfigJson['debugMode']
 versionNumber = loadedConfigJson['version']
 minRandomAmount = loadedConfigJson['minRandomAmount']
 maxRandomAmount = loadedConfigJson['maxRandomAmount']
-currentSelectedMode = loadedConfigJson['currentMode']
+currentSelectedMode = loadedConfigJson['currentMode'].casefold() # Set in config.json. Types are default (picks 1) and vs (picks 2, meant for tournaments)
 showWelcomeMessage = loadedConfigJson['showWelcomeMessage']
 i = 0
 os.system('color')
@@ -39,21 +39,48 @@ if debugKaraokeMode == True:
     input("Press any button to continue")
 else:
     pass
+if currentSelectedMode == "default":
+    while (i < randomWhileAmount):
+        i += 1
+        selectedChoiceData = random.choice(loadedNumberListData)
+        if debugKaraokeMode == True:
+            cprint(f"While loop has run {i}/{randomWhileAmount} times and has selected {selectedChoiceData}", "blue")
 
-while (i < randomWhileAmount):
-    i += 1
-    selectedChoiceData = random.choice(loadedNumberListData)
-    if debugKaraokeMode == True:
-        cprint(f"While loop has run {i}/{randomWhileAmount} times and has selected {selectedChoiceData}", "blue")
+    root = Tk()
+    root.withdraw()
+    # root.geometry("300x200")
+    messagebox.showinfo(f"Selected {selectedChoiceData}", f"Selected: {selectedChoiceData}.\npress ok to remove {selectedChoiceData} from list") 
+    print(f"Selected {selectedChoiceData}")
 
-root = Tk()
-root.withdraw()
-# root.geometry("300x200")
-messagebox.showinfo(f"Selected {selectedChoiceData}", f"Selected: {selectedChoiceData}.\npress ok to remove {selectedChoiceData} from list") 
-print(f"Selected {selectedChoiceData}")
+    removedNumberList = loadedNumberListData.remove(selectedChoiceData)
+    newJsonData = dict(data = removedNumberList)
+    with open("data.json", "w") as outfile:
+        json.dump(loadedNumberJson, outfile, indent=4, sort_keys=True)
+    print(f"{selectedChoiceData} has been removed from list")
+elif currentSelectedMode == "vs":
+    while (i <= randomWhileAmount):
+        i += 1
+        selectedChoiceData = random.choice(loadedNumberListData)
+        selectedChoiceData2 = random.choice(loadedNumberListData)
+        if (i == randomWhileAmount) and (selectedChoiceData == selectedChoiceData2):
+            i -= 1
+        if debugKaraokeMode == True:
+            cprint(f"While loop has run {i}/{randomWhileAmount} times and has selected {selectedChoiceData}", "blue")
 
-removedNumberList = loadedNumberListData.remove(selectedChoiceData)
-newJsonData = dict(data = removedNumberList)
-with open("data.json", "w") as outfile:
-    json.dump(loadedNumberJson, outfile, indent=4, sort_keys=True)
-print(f"{selectedChoiceData} has been removed from list")
+    root = Tk()
+    root.withdraw()
+    # root.geometry("300x200")
+    messagebox.showinfo(f"{selectedChoiceData} VS {selectedChoiceData2}", f"{selectedChoiceData} VS {selectedChoiceData2}.\npress ok to remove names from list") 
+    print(f"{selectedChoiceData} VS {selectedChoiceData2}")
+
+    removedNumberList = loadedNumberListData.remove(selectedChoiceData)
+    removedNumberList2 = loadedNumberListData.remove(selectedChoiceData2)
+    newJsonData = dict(data = removedNumberList2)
+    with open("data.json", "w") as outfile:
+        json.dump(loadedNumberJson, outfile, indent=4, sort_keys=True)
+    print(f"{selectedChoiceData} and {selectedChoiceData2} have been removed from list")
+else:
+    root = Tk()
+    root.withdraw()
+    messagebox.showerror("ERROR", f"Current Selected Mode {currentSelectedMode} IS NOT RECOGNIZED!\nChange in config.json")
+    sys.exit(f"Current Selected Mode {currentSelectedMode} IS NOT RECOGNIZED!\nChange in config.json")
